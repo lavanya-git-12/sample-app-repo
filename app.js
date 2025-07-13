70b84ea4acd307ac76a0845477437e065404be7b
Your Name <you@example.com>
2025-07-13 12:01:13 +0000

    Update app to V1 and trigger initial GitHub CI/CD deployment

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  const message = process.env.MESSAGE || 'Hello from the sample app!';
+  const message = process.env.MESSAGE || 'Hello from the GitHub-CI/CD-ed sample app - V1!';
   const hostname = require('os').hostname();
   res.send(`${message} (served by ${hostname})\n`);
 });
